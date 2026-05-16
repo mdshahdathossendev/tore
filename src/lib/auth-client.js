6882e80a8491835ac6ed33133ad0b99ccc9b0695
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-05-16 07:50:09 +0600

    link add

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { jwtClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
    
-    baseURL: "http://localhost:3000",
+    baseURL: "https://tore-eta.vercel.app",
     plugins: [
         jwtClient()
     ]
